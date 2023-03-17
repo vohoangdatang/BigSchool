d80076210c10a99a184930cdcf665855dd52439d
Datvh1422 <kichban.iteam@gmail.com>
2023-03-17 16:00:27 +0700

    Update den trang 31

--- VoHoangDat_1811060166/Controllers/HomeController.cs
+++ VoHoangDat_1811060166/Controllers/HomeController.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using VoHoangDat_1811060166.Models;
 
 namespace VoHoangDat_1811060166.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController ()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.Datetime > DateTime.Now);
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
