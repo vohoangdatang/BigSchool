9ad118f62449384a825c7a37dc5b0c6f12ea49f8
Datvh1422 <kichban.iteam@gmail.com>
2023-03-21 18:13:14 +0700

    Update toi trang 39

---
+++ VoHoangDat_1811060166/Controllers/AttendancesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+using VoHoangDat_1811060166.Models;
+
+namespace VoHoangDat_1811060166.Controllers
+
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
+
--- VoHoangDat_1811060166/Models/AccountViewModels.cs
+++ VoHoangDat_1811060166/Models/AccountViewModels.cs
@@ -79,7 +79,12 @@ namespace VoHoangDat_1811060166.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
+
 
     public class ResetPasswordViewModel
     {
